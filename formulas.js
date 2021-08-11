f88fed2410f1a08623f3cdcf42702b02ca474a80
mramiro <manuel.ramiro.zabaleta@gmail.com>
2021-08-11 17:21:14 +0200

    Primer save, añadidos los botones de primera inversion, numeros de flujo y creación de la formula

---
+++ formulas.js
@@ -0,0 +1,24 @@
+
+
+
+function nflujo(){
+    for(var = f, )
+
+}
+
+
+
+function formulaVan(i0, ft, k){
+    var resultadoVan = - i0 + (ft / (1 + k)**n)
+
+}
+
+function calculo(){
+    const inputInversion0 = document.getElementById("inputInversion0");
+    const i0Value = inputInversion0.value;
+
+
+
+
+    const resultadoVan = formulaVan(i0Value, );
+}
